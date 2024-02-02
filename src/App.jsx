45046a7e100a5e60229f516ed7767973ed6d208b
virginia campillo <vircampillo22@gmail.com>
2024-02-02 20:42:26 +0100

    agregamos una lista con conceptos de react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Informacion from "./component/Informacion"
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+
+
+  return (
+    <section className="container my-4" >
+      <h1 className="text-center display-3" >PrimerApp react</h1>
+      <Informacion></Informacion>
+    </section>
+    
+  )
+}
+
+export default App
---
+++ src/component/Informacion.jsx
@@ -0,0 +1,43 @@
+const Informacion = () => {
+  return (
+    <section>
+      <h2>Conceptos basicos de bootstrap</h2>
+      <p>
+        React (también llamada React.js o ReactJS) es una librería Javascript de
+        código abierto diseñada para crear interfaces de usuario con el objetivo
+        de facilitar el desarrollo de aplicaciones en una sola página. Es
+        mantenido por Facebook y la comunidad de software libre. En el proyecto
+        hay más de mil desarrolladores libres.
+      </p>
+      <ol className="list-group" >
+        <li className="list-group-item">
+          Los componentes permiten separar la interfaz de usuario en piezas
+          independientes, reutilizables y pensar en cada pieza de forma aislada
+        </li>
+        <li className="list-group-item">
+          Las props son la colección de datos que un componente recibe del
+          contenedor padre, y que pueden usarse para definir los elementos de
+          React que retornará el componente. En términos prácticos, si un
+          componente necesita recibir información para funcionar, la recibe vía
+          props.
+        </li>
+        <li className="list-group-item">
+          state representan los valores renderizados, es decir, lo que hay
+          actualmente en la pantalla.
+        </li>
+        <li className="list-group-item">
+          el término “ciclo de vida” o lifecycle , se refiere a las diferentes
+          fases por los que pasa un componente desde su creación hasta su
+          destrucción.
+        </li>
+        <li className="list-group-item">
+          JSX (JavaScript XML) es una extensión sintáctica para JavaScript que
+          permite a los desarrolladores escribir código similar al HTML dentro
+          de un archivo JavaScript.
+        </li>
+      </ol>
+    </section>
+  );
+};
+
+export default Informacion;
