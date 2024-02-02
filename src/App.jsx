d17cc87394fcb173a5fbacd4e95d5a4d2300d687
virginia campillo <vircampillo22@gmail.com>
2024-02-02 21:59:53 +0100

    agregamos un contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Contador from "./component/Contador";
 import Informacion from "./component/Informacion"
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <section className="container my-4" >
       <h1 className="text-center display-3" >PrimerApp react</h1>
       <Informacion tituloSecundario="Este titulo fue creado con un props"></Informacion>
+      <Contador></Contador>
     </section>
     
   )
---
+++ src/component/Contador.jsx
@@ -0,0 +1,14 @@
+const Contador = () => {
+    return (
+        <article className="text-center mt-4">
+            <h3>Contador</h3>
+            <p className="fw-bold">10</p>
+            <button className="btn btn-primary">+1</button>
+        </article>
+        
+
+
+    );
+};
+
+export default Contador;
